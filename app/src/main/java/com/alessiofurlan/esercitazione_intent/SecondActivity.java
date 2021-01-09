efb0dece7c98714806a06d22cdd5c1f91ec4268a
Federico <carrabba.federico@gmail.com>
2021-01-09 13:37:49 +0100

    INTENT

--- app/src/main/java/com/alessiofurlan/esercitazione_intent/SecondActivity.java
+++ app/src/main/java/com/alessiofurlan/esercitazione_intent/SecondActivity.java
@@ -2,7 +2,9 @@ package com.alessiofurlan.esercitazione_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String nome = intent.getStringExtra("NOME");
+        String cognome = intent.getStringExtra("COGNOME");
+        String anni = intent.getStringExtra("ANNI");
+        String mail = intent.getStringExtra("EMAIL");
+        String telefono = intent.getStringExtra("TELEFONO");
+        String sito = intent.getStringExtra("SITO");
+
+        Contatto contatto = (Contatto)getIntent().getSerializableExtra("contatto");
+
     }
 }
