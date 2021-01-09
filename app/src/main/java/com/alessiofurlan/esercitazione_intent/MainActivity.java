9ea6e3db9e3678890a5bb688f50b3a6a46d9b55c
HyperEmme <mussio.michele@gmail.com>
2021-01-09 12:57:28 +0100

    Controllo compilazione campi

--- app/src/main/java/com/alessiofurlan/esercitazione_intent/MainActivity.java
+++ app/src/main/java/com/alessiofurlan/esercitazione_intent/MainActivity.java
@@ -3,12 +3,38 @@ package com.alessiofurlan.esercitazione_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtNome;
+    EditText edtCognome;
+    EditText edtTel;
+    EditText edtWeb;
+    EditText edtMail;
+    EditText edtAnni;
+
+    Button btnInvia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtNome = findViewById(R.id.edtNome);
+        edtCognome = findViewById(R.id.edtCognome);
+        edtAnni = findViewById(R.id.edtAnni);
+        edtMail = findViewById(R.id.edtEmail);
+        edtTel = findViewById(R.id.edtTel);
+        edtWeb = findViewById(R.id.edtWeb);
+    }
+    public boolean Controllo()
+    {
+        boolean controllo=false;
+        if(edtNome.getText().toString()=="" || edtCognome.getText().toString()=="" || edtTel.getText().toString()=="" || edtWeb.getText().toString()=="" || edtMail.getText().toString()=="" || edtAnni.getText().toString()=="")
+            return controllo=false;
+        else
+            return controllo=true;
     }
 }
