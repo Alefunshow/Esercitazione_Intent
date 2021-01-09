fac22eb890cf8cc00181abb931dde77097b3216b
Alessio Furlan <alessio.furlan1@gmail.com>
2021-01-09 14:12:28 +0100

    Grafica base SecondActivity e Tre funzioni chiamata, email, sito Web

--- app/src/main/java/com/alessiofurlan/esercitazione_intent/SecondActivity.java
+++ app/src/main/java/com/alessiofurlan/esercitazione_intent/SecondActivity.java
@@ -5,16 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Contatto c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Contatto c = (Contatto)getIntent().getSerializableExtra("contatto");
-        Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:3356897542"+c.telefono));
+         c = (Contatto)getIntent().getSerializableExtra("contatto");
+    }
+
+    public void Chiama(View v){
+        Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+c.telefono));
         startActivity(call);
     }
+    public void Email(View v){
+
+    }
+    public void SitoWeb(View v){
+
+    }
+
 }
