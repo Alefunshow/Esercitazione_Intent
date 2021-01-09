2884180d579931facf681dbc1cece493f713224c
Federico <carrabba.federico@gmail.com>
2021-01-09 12:55:58 +0100

    aggiunta editText

--- app/src/main/java/com/alessiofurlan/esercitazione_intent/MainActivity.java
+++ app/src/main/java/com/alessiofurlan/esercitazione_intent/MainActivity.java
@@ -1,8 +1,11 @@
 package com.alessiofurlan.esercitazione_intent;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nome = findViewById(R.id.txtNome);
+        EditText cognome = findViewById(R.id.txtCognome);
+        EditText anni = findViewById(R.id.txtAnni);
+        EditText sitoWeb = findViewById(R.id.txtSitoWeb);
+        EditText email = findViewById(R.id.txtEmail);
+        EditText telefono = findViewById(R.id.txtTelefono);
+
     }
 }
